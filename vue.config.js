edb1b930547504cefe52b72b9cfad915cd91cc91
mao <2041353520@qq.com>
2022-09-06 11:18:03 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const cpns = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭eslint检查, 编译时候的eslint的检查
+  lintOnSave: false,
+
+  // 原生webpack
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    },
+
+    // 路径别名
+    resolve: {
+      alias: {
+        cpns,
+        views
+      }
+    }
+  }
 })
